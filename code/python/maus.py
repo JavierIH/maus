bc0a16b9c18a75460255b9f9a3d79e8118ed87e2
JavierIH <javier.i.h@hotmail.com>
2015-09-18 17:23:50 +0200

    Implemented roll feedback for dinamic walking

--- code/python/maus.py
+++ code/python/maus.py
@@ -7,20 +7,20 @@ from libraries.bno055.bno055 import Inclinometer
 
 calibration=0
 
-AMP=25
-servo1 = octosnake.Oscillator(1600, AMP, 0, -45, -15)
-servo2 = octosnake.Oscillator(1600, AMP, 0, 45, 8)
-servo3 = octosnake.Oscillator(1600, AMP, 90, 30, 0)
-servo4 = octosnake.Oscillator(1600, AMP, 90, -30, -18)
-servo5 = octosnake.Oscillator(1600, 110, 180, 0, 21)
+#AMP=25
+#servo1 = octosnake.Oscillator(1600, AMP, 0, -45, -15)
+#servo2 = octosnake.Oscillator(1600, AMP, 0, 45, 8)
+#servo3 = octosnake.Oscillator(1600, AMP, 90, 30, 0)
+#servo4 = octosnake.Oscillator(1600, AMP, 90, -30, -18)
+#servo5 = octosnake.Oscillator(1600, 110, 180, 0, 21)
 
-#AMP=15
-#T=800
-#servo1 = octosnake.Oscillator(T, AMP, 0, -60-calibration, -15)
-#servo2 = octosnake.Oscillator(T, AMP, 0, 60+calibration, 8)
-#servo3 = octosnake.Oscillator(T, AMP, 90, 30-calibration, 0)
-#servo4 = octosnake.Oscillator(T, AMP, 90, -30+calibration, -18)
-#servo5 = octosnake.Oscillator(T, 60, 220, 0, 21)
+AMP=25
+T=1000
+servo1 = octosnake.Oscillator(T, AMP, 0, -60-calibration, -15)
+servo2 = octosnake.Oscillator(T, AMP, 0, 60+calibration, 8)
+servo3 = octosnake.Oscillator(T, AMP, 90, 30-calibration, 0)
+servo4 = octosnake.Oscillator(T, AMP, 90, -30+calibration, -18)
+servo5 = octosnake.Oscillator(T, 60, 220, 0, 21)
 
 servo2.ref_time = servo1.ref_time
 servo3.ref_time = servo1.ref_time
@@ -41,24 +41,25 @@ sensor=Inclinometer(bus, bno055_address)
 def test():
 	ref=time.time()
 	while True:
-		#position1 = servo1.refresh()
-                #position2 = servo2.refresh()
-                #position3 = servo3.refresh()
-		#position4 = servo4.refresh()
-		#position5 = servo5.refresh()
+		position1 = servo1.refresh()
+                position2 = servo2.refresh()
+                position3 = servo3.refresh()
+		position4 = servo4.refresh()
+		position5 = servo5.refresh()
 
  		try:
-			#control.move(8, position1)
-			#control.move(9, position2)
-			#control.move(10, position3)
-                	#control.move(11, position4)
-			control.move(4, -sensor.get_roll()+21)
+                        data = sensor.get_roll()
+			control.move(8, position1 - data/2)
+			control.move(9, position2 - data/2)
+			control.move(10, position3 + data/2)
+                	control.move(11, position4 + data/2)
+			control.move(4, position5 - data*3)
+                        print data
 		except IOError:
 			bus = smbus.SMBus(0)
 			#print '\n'*8, '------------- I2C reiniciado -------------'
 
 		#print 'Servo 1:  ' + str(position1) + '    \t Servo 2: ' + str(position2) + '    \t Servo 3: ' + str(position3) + '    \t Servo 4: '  + str(position4)
-		#time.sleep(0.1)
-		print sensor.get_roll()
+		#time.sleep(1)
 
 test()
