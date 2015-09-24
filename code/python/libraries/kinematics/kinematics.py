a2452341f56692ac090bb32cddab0181bb0eacc4
JavierIH <javier.i.h@hotmail.com>
2015-09-24 19:10:12 +0200

    New gaits using inverse kinematics

--- code/python/libraries/kinematics/kinematics.py
+++ code/python/libraries/kinematics/kinematics.py
@@ -4,22 +4,22 @@ import numpy as np
 class MausKinematics(object):
 
     def __init__(self):
-        self.left_leg = kdl.Chain()
-        self.left_leg.addSegment(kdl.Segment(kdl.Joint(kdl.Joint.RotY), kdl.Frame(kdl.Vector(0,0,-50))))
-        self.left_leg.addSegment(kdl.Segment(kdl.Joint(kdl.Joint.RotY), kdl.Frame(kdl.Vector(0,0,-50))))
+        self.leg = kdl.Chain()
+        self.leg.addSegment(kdl.Segment(kdl.Joint(kdl.Joint.RotY), kdl.Frame(kdl.Vector(0,0,-50))))
+        self.leg.addSegment(kdl.Segment(kdl.Joint(kdl.Joint.RotY), kdl.Frame(kdl.Vector(0,0,-50))))
 
-        self.ik_solver = kdl.ChainIkSolverPos_LMA(self.left_leg)
+        self.ik_solver = kdl.ChainIkSolverPos_LMA(self.leg)
 
-	self.left_alpha1=45
-	self.left_alpha2=-45
-	self.left_beta=self.left_alpha2-self.left_alpha1
+	self.alpha1=45
+	self.alpha2=-45
+	self.beta=self.alpha2-self.alpha1
 
     def getJoints(self, x, y, z):
         target_frame = kdl.Frame(kdl.Vector(x,y,z))
-	current_angles = kdl.JntArray(self.left_leg.getNrOfJoints())
-	result_angles = kdl.JntArray(self.left_leg.getNrOfJoints())
-        current_angles[0] = np.deg2rad(self.left_alpha1)
-        current_angles[1] = np.deg2rad(self.left_beta)
+	current_angles = kdl.JntArray(self.leg.getNrOfJoints())
+	result_angles = kdl.JntArray(self.leg.getNrOfJoints())
+        current_angles[0] = np.deg2rad(self.alpha1)
+        current_angles[1] = np.deg2rad(self.beta)
 
         self.ik_solver.CartToJnt(current_angles, target_frame, result_angles)
 	result_angles[1] = result_angles[0] + result_angles[1]
--- code/python/maus.py
+++ code/python/maus.py
@@ -3,38 +3,26 @@ import libraries.octosnake.octosnake as octosnake
 import smbus
 from libraries.pca9865.pca9865 import ServoController
 from libraries.bno055.bno055 import Inclinometer
+from libraries.kinematics.kinematics import MausKinematics
 
-#AMP=25
-#servo1 = octosnake.Oscillator(1600, AMP, 0, -45, -15)
-#servo2 = octosnake.Oscillator(1600, AMP, 0, 45, 8)
-#servo3 = octosnake.Oscillator(1600, AMP, 90, 30, 0)
-#servo4 = octosnake.Oscillator(1600, AMP, 90, -30, -18)
-#servo5 = octosnake.Oscillator(1600, 110, 180, 0, 21)
-
-#AMP=25
-#T=1000
-#servo1 = octosnake.Oscillator(T, AMP, 0, -60, -15)
-#servo2 = octosnake.Oscillator(T, AMP, 0, 60, 8)
-#servo3 = octosnake.Oscillator(T, AMP, 90, 30, 0)
-#servo4 = octosnake.Oscillator(T, AMP, 90, -30, -18)
-#servo5 = octosnake.Oscillator(T, 60, 220, 0, 21)
-
-AMP=25
+osc = []
+xamp_left = 10 
+xamp_right = 20
+zamp = 30
 T=1000
-ajuste = 0
+osc.append(octosnake.Oscillator(T, xamp_left, 0, -20))
+osc.append(octosnake.SemiSine(T, zamp, 90, -75))
+osc.append(octosnake.Oscillator(T, xamp_right, 180, -20))
+osc.append(octosnake.SemiSine(T, zamp, 270, -75))
+osc.append(octosnake.Oscillator(T, 70, 335, 0))
+#osc.append(octosnake.Oscillator(T*14, 10, 0, 10))
 
-osc=[] 
-
-osc.append(octosnake.Oscillator(T, AMP, 0, -60))
-osc.append(octosnake.Oscillator(T, AMP, 0, 60))
-osc.append(octosnake.Oscillator(T, AMP, 90, 30))
-osc.append(octosnake.Oscillator(T, AMP, 180, -30))
-osc.append(octosnake.Oscillator(T, 60, 190, 0))
 
 osc[1].ref_time = osc[0].ref_time
 osc[2].ref_time = osc[0].ref_time
 osc[3].ref_time = osc[0].ref_time
 osc[4].ref_time = osc[0].ref_time
+
 
 bus = smbus.SMBus(0)
 
@@ -44,8 +32,10 @@ bno055_address = 0x29
 if not bus:
     raise Exception('I2C bus connection failed!')
 
-control=ServoController(bus, pca9865_address)
 sensor=Inclinometer(bus, bno055_address)
+control=ServoController(bus, pca9865_address)
+
+ik = MausKinematics()
 
 control.addServo(8, -15)
 control.addServo(9, 8)
@@ -53,25 +43,37 @@ control.addServo(10, 0)
 control.addServo(11, -18)
 control.addServo(4, 21)
 
+control.servos[9].reverse = True
+control.servos[11].reverse = True
+
+
 def test():
-	ref=time.time()
-	while True:
-		for i in range(len(osc)):
-                    osc[i].refresh()
-                
- 		try:
-                        roll_data = 0#sensor.get_roll()
-			control.move(8, osc[0].output - roll_data/2)
-			control.move(9, osc[1].output - roll_data/2)
-			control.move(10, osc[2].output + roll_data/2)
-                	control.move(11, osc[3].output + roll_data/2)
-			control.move(4, osc[4].output)# - roll_data*3)
-                        #print roll_data, '\t', pitch_data
-		except IOError:
-			bus = smbus.SMBus(0)
-			#print '\n'*8, '------------- I2C reiniciado -------------'
+    #var_phase = 250
+    while True:
+	roll=sensor.get_roll()
+        for i in range(len(osc)):
+            osc[i].refresh()
+        try:
+            left_joints = ik.getJoints(osc[0].output, 0, osc[1].output-roll/7)
+            right_joints = ik.getJoints(osc[2].output, 0, osc[3].output+roll/7)
+            control.move(8, left_joints[0])
+            control.move(10, left_joints[1])
+            control.move(9, right_joints[0])
+            control.move(11, right_joints[1])
+            control.move(4, osc[4].output - 0.5*sensor.get_roll())
+            
+            #var_phase+=0.1
+            #osc[4].phase=var_phase
+            #print '\n\n\nleft: ', osc[0].output, '\tq0: ', left_joints[0], '\tq1', left_joints[1]
+            #print 'right: ', osc[2].output, '\tq0: ', right_joints[0], '\tq1', right_joints[1]
+            #print 'phase: ', osc[4].phase
+
+        except IOError:
+            bus = smbus.SMBus(0)
+		#print '\n'*8, '------------- I2C reiniciado -------------'
 
 		#print 'Servo 1:  ' + str(position1) + '    \t Servo 2: ' + str(position2) + '    \t Servo 3: ' + str(position3) + '    \t Servo 4: '  + str(position4)
 		#time.sleep(1)
 
-#test()
+test()
+#control.sleep()
